c9474ad2b30818869d33de7716e0f81521d509fa
Nicolas <nicolas.yang.1@gmail.com>
2020-03-30 10:53:26 -0300

    Add portfolio Rock Paper Scissors.

---
+++ portfolio/rockPaperScissors/js/script.js
@@ -0,0 +1,57 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+    
+    alert("Chose your weapon! And play");
+
+
+let player, robot, choices = ["ROCK", "PAPER", "SCISSORS"]
+
+const rock = document.querySelector('#rock');
+rock.addEventListener('click', () => {
+    player = "ROCK";
+})
+const paper = document.querySelector("#paper");
+paper.addEventListener('click', () => {
+    player = "PAPER";
+})
+const scissors = document.querySelector("#scissors");
+scissors.addEventListener('click', () => {
+    player = "SCISSORS";
+})
+
+
+
+let playRound = () => {
+    robot = choices[Math.round(Math.random() * 2)];
+
+    if (robot == "ROCK" && player == "ROCK") {
+        return "Drawww.";
+    } else if (robot == "ROCK" && player == "PAPER") {
+        return "You Win!";
+    } else if (robot == "ROCK" && player == "SCISSORS") {
+        return "You Lose!";
+    } else if (robot == "PAPER" && player == "ROCK") {
+        return "You Lose!";
+    } else if (robot == "PAPER" && player == "PAPER") {
+        return "Drawww.";
+    } else if (robot == "PAPER" && player == "SCISSORS") {
+        return "You Win!";
+    } else if (robot == "SCISSORS" && player == "ROCK") {
+        return "You Win!";
+    } else if (robot == "SCISSORS" && player == "PAPER") {
+        return "You Lose!";
+    } else if (robot == "SCISSORS" && player == "SCISSORS") {
+        return "Drawww.";
+    } else {
+        return "If you don't want to play, don't play!"
+    }
+}
+
+const play = document.querySelector('#play');
+play.addEventListener('click', () => {
+    {
+        playRound()
+        alert("You choose " + player + " and robot choose " + robot + ".")
+        alert(playRound())}
+})
+})
