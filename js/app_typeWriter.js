39003fb4a6a5c28d07020cb8a64e65e329b9cc43
NicoLarson <nicolas.yang.1@gmail.com>
2020-05-28 16:23:09 +0200

    Add typeWriter

--- js/app_typeWriter.js
+++ js/app_typeWriter.js
@@ -1,28 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    let elTitle = document.querySelector('section>dl>dt');
-    let elIam =  document.querySelector('section>dl>dd');
-    var pos = 0;
-    let pos2 = 0
-    let hello = "Bonjour!";
-    let iAm = "Je suis étudiant en programmation."
-    var speed = 200;
-
-    setTimeout(typeWriter, speed);
-    setTimeout(typeWriterb,speed)
-    function typeWriter() {
-        if (pos < hello.length) {
-            elTitle.innerHTML += hello.charAt(pos);
-            pos++;
-            setTimeout(typeWriter, speed);
-        }
-    }
-    function typeWriterb() {
-        if (pos2 < iAm.length) {
-            elIam.innerHTML += iAm.charAt(pos2);
-            pos2++;
-            setTimeout(typeWriterb, speed);
-        }
-    }
-
+    var app = document.getElementById('type');
+    var typewriter = new Typewriter(app);
+    
+    typewriter.typeString('Bonjour! <br> Je suis étudiant en programmation.')
+    .start();
+ 
 })
