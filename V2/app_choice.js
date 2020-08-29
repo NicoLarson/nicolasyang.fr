6f33f987806e5219c31dd3082613d92a97167582
Nicolas Yang <nicolas.yang.1@gmail.com>
2020-08-30 00:32:23 +0200

    Test red-pill welcome choice

---
+++ V2/app_choice.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+  
+    let welcome = document.querySelector('#welcome p')
+    let welcomeWriter = new Typewriter(welcome, {
+        delay: 50
+    })
+    welcomeWriter
+        .typeString(`Wake up...<br>
+        The Matrix has you...<br>
+        Take the red pill, you stay in wonderland. And I show you how deep the rabbit-hole goes.<br>
+        Take the blue pill. The story ends. You wake up in your bed and believe whatever you want to
+                    believe.`)
+        .start()
+
+
+        let header = document.querySelector('header')
+        let secWelcome = document.querySelector('#welcome')
+let redPill = document.querySelector('#red-pill')
+redPill.addEventListener('click',()=>{
+header.classList.remove('disapear')
+secWelcome.classList.add('disapear')
+})
+
+})
