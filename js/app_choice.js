335334004b30000d8fea2a45c5f3378703a1318c
Nicolas Yang <nicolas.yang.1@gmail.com>
2020-08-31 13:32:44 +0200

    add neo style css

--- js/app_choice.js
+++ js/app_choice.js
@@ -4,11 +4,21 @@ document.addEventListener("DOMContentLoaded", () => {
     let welcomeWriter = new Typewriter(welcome, {
         delay: 50
     })
-    welcomeWriter
-        .typeString(`Je suppose que pour l'instant tu te sens un peu comme Alice, tombée dans le terrier du lapin blanc.<br>
-        Crois-tu en la destinée ?<br>
+    const mediaQueryList = window.matchMedia("(max-height: 568px)");
+    if (mediaQueryList.matches) {
+        welcomeWriter
+            .typeString(`> Crois-tu en la destinée ?<br>
         Choisis la pilule bleue et tout s'arrête, après tu pourra faire de beaux rêves et penser ce que tu veux.<br> 
         Choisis la pilule rouge, tu restes au pays des merveilles et on descend avec le lapin blanc au fond du gouffre.`)
-        .start()
+            .start()
+    } else {
+        welcomeWriter
+            .typeString(`
+            > Je suppose que pour l'instant tu te sens un peu comme Alice, tombée dans le terrier du lapin blanc.<br>
+            > Crois-tu en la destinée ?<br>
+            > Choisis la pilule bleue et tout s'arrête, après tu pourra faire de beaux rêves et penser ce que tu veux.<br> 
+            > Choisis la pilule rouge, tu restes au pays des merveilles et on descend avec le lapin blanc au fond du gouffre.`)
+            .start()
+    }
 
 })
