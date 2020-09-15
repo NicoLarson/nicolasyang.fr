a56bfaf3d8d53e49ed8897d2c303896758235ce0
Nicolas Yang <nicolas.yang.1@gmail.com>
2020-09-15 17:09:55 +0200

    Correction orthographe

--- js/app_choice.js
+++ js/app_choice.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
         message.style.cssText = 'font-size: 1rem; padding: 1rem;'
         messageWriter
             .typeString(`> Crois-tu en la destinée ?<br>
-    > Choisis la pilule bleue et tout s'arrête, après tu pourra faire de beaux rêves et penser ce que tu veux.<br> 
+    > Choisis la pilule bleue et tout s'arrête, après tu pourras faire de beaux rêves et penser ce que tu veux.<br> 
     > Choisis la pilule rouge, tu restes au pays des merveilles et on descend avec le lapin blanc au fond du gouffre.`)
             .start()
 
