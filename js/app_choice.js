f9b4c460dd11a12c2ed6cae7ef07d3106630090b
Nicolas Yang <nicolas.yang.1@gmail.com>
2020-09-21 20:34:05 +0200

    Correction faute

--- js/app_choice.js
+++ js/app_choice.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
         message.style.cssText = 'font-size: 1rem; padding: 1rem;'
         messageWriter
             .typeString(`> Crois-tu en la destinée ?<br>
-    > Choisis la pilule bleue et tout s'arrête, après tu pourras faire de beaux rêves et penser ce que tu veux.<br> 
+    > Choisis la pilule bleue et tout s'arrête, après tu pourrass faire de beaux rêves et penser ce que tu veux.<br> 
     > Choisis la pilule rouge, tu restes au pays des merveilles et on descend avec le lapin blanc au fond du gouffre.`)
             .start()
 
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
             .typeString(`
             > Je suppose que pour l'instant tu te sens un peu comme Alice, tombée dans le terrier du lapin blanc.<br>
             > Crois-tu en la destinée ?<br>
-        > Choisis la pilule bleue et tout s'arrête, après tu pourra faire de beaux rêves et penser ce que tu veux.<br> 
+        > Choisis la pilule bleue et tout s'arrête, après tu pourras faire de beaux rêves et penser ce que tu veux.<br> 
         > Choisis la pilule rouge, tu restes au pays des merveilles et on descend avec le lapin blanc au fond du gouffre.`)
             .start()
 
@@ -27,7 +27,7 @@ document.addEventListener("DOMContentLoaded", () => {
             .typeString(`
             > Je suppose que pour l'instant tu te sens un peu comme Alice, tombée dans le terrier du lapin blanc.<br>
             > Crois-tu en la destinée ?<br>
-            > Choisis la pilule bleue et tout s'arrête, après tu pourra faire de beaux rêves et penser ce que tu veux.<br> 
+            > Choisis la pilule bleue et tout s'arrête, après tu pourras faire de beaux rêves et penser ce que tu veux.<br> 
             > Choisis la pilule rouge, tu restes au pays des merveilles et on descend avec le lapin blanc au fond du gouffre.`)
             .start()
     }
